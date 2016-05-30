3214c1f24db36f8fe9372546bb1f2c5d5843b741
nickciava98 <n.ciavarella@outlook.it>
2016-05-30 15:56:27 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 				<table>
 					<tr>
 						<td>
-							<img src = 'python.png' width = '100' height = '100'>
+							<a href = 'http://www.python.it/' target = '_blank'>
+								<img src = 'python.png' width = '100' height = '100'>
+							</a>
 						</td>
 						
 						<td>
@@ -19,14 +21,38 @@
 						</td>
 						
 						<td>
-							<img src = 'windows.jpg' width = '100' height = '100'>
+							<a href = 'https://www.microsoft.com/it-it/windows/' target = '_blank'>
+								<img src = 'windows.jpg' width = '100' height = '100'>
+							</a>
 						</td>
 					</tr>
 				</table>
 				
+				<a href = 'http://www.gnu.org/licenses/agpl-3.0.txt' target = '_blank'>
+					<img src = 'agplv3.png'>
+				</a>
+				
+				<br><br>
+				
 				<font face = 'Arial'>
 					Crea il file .exe per il tuo software .py
 				</font>
+				
+				<br><br>
+				
+				<font face = 'Arial'>
+					<b>
+						<em>
+							Powered by
+						</em>
+					</b>
+				</font>
+				
+				<br>
+				
+				<a href = 'http://www.pyinstaller.org/' target = '_blank'>
+					<img src = 'pyinstaller.png'>
+				</a>
 				
 				<br><br>
 				
@@ -49,16 +75,59 @@
 					
 					<br>
 					
+					<font face = 'Arial'>
+						Tipo software
+					</font>
+					
+					<br>
+					
+					<table>	
+						<tr>
+							<td>
+								<font face = 'Arial'>
+									Riga di comando
+								</font>
+							</td>
+							
+							<td>
+								<input type = 'radio' name = 'tipo' value = 'cli'>
+							</td>
+						</tr>
+						
+						<tr>
+							<td>
+								<font face = 'Arial'>
+									Grafica
+								</font>
+							</td>
+							
+							<td>
+								<input type = 'radio' name = 'tipo' value = 'gui'>
+							</td>
+						</tr>	
+					</table>
+					
+					<br>
+					
 					<input style = 'width: 115px' type = 'submit' name = 'invia' value = 'INVIA'>
 				</form>");
 	
-	if(isset($_POST["invia"]))
+	if(isset($_POST["invia"]) and isset($_POST["tipo"]))
 	{
 		if(strpos($_FILES["file"]["name"], ".py") !== false)
 		{			
 			move_uploaded_file($_FILES["file"]["tmp_name"], "C:/".$_FILES["file"]["name"]);
 			
-			print(shell_exec("cd C:/ && pyinstaller --onefile ".$_FILES["file"]["name"]));
+			if(strcmp($_POST["tipo"], "cli") == 0)
+			{
+				print(shell_exec("cd C:/ && pyinstaller --onefile ".$_FILES["file"]["name"]));	
+			}
+			
+			if(strcmp($_POST["tipo"], "gui") == 0)
+			{
+				print(shell_exec("cd C:/ && pyinstaller --onefile --windowed ".$_FILES["file"]["name"]));
+			}
+			
 			
 			$name = "";
 			
