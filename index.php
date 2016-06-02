ef439223136a1ff4acdc382e47f0a7cd11c16293
nickciava98 <n.ciavarella@outlook.it>
2016-06-02 21:37:22 +0200

    Add files via upload
    Final release.

--- index.php
+++ index.php
@@ -2,115 +2,137 @@
 	print("	<title>
 				WinPy32
 			</title>
-			
-			<center>		
-				<table>
-					<tr>
-						<td>
-							<a href = 'http://www.python.it/' target = '_blank'>
-								<img src = 'python.png' width = '100' height = '100'>
-							</a>
-						</td>
-						
-						<td>
-							<h1>
-								<font face = 'Arial'>
-									WinPy32
-								</font>
-							</h1>
-						</td>
-						
-						<td>
-							<a href = 'https://www.microsoft.com/it-it/windows/' target = '_blank'>
-								<img src = 'windows.jpg' width = '100' height = '100'>
-							</a>
-						</td>
-					</tr>
-				</table>
-				
-				<a href = 'http://www.gnu.org/licenses/agpl-3.0.txt' target = '_blank'>
-					<img src = 'agplv3.png'>
-				</a>
-				
-				<br><br>
-				
-				<font face = 'Arial'>
-					Crea il file .exe per il tuo software .py
-				</font>
-				
-				<br><br>
-				
-				<font face = 'Arial'>
-					<b>
-						<em>
-							Powered by
-						</em>
-					</b>
-				</font>
-				
-				<br>
-				
-				<a href = 'http://www.pyinstaller.org/' target = '_blank'>
-					<img src = 'pyinstaller.png'>
-				</a>
-				
-				<br><br>
-				
-				<form action = '' method = 'post' enctype = 'multipart/form-data'>
-					<input type = 'hidden' name = 'MAX_FILE_SIZE' value = '30000'>
-					
+			<body bgcolor = '#F5F5DC'>
+				<center>		
 					<table>
 						<tr>
 							<td>
-								<font face = 'Arial'>
-									Carica il file:
-								</font>
+								<a title = 'Python 3.5' href = 'http://www.python.it/' target = '_blank'><img src = 'python.png' width = '90' height = '90'></a>
 							</td>
 							
 							<td>
-								<input type = 'file' name = 'file'>
+								<h1>
+									<span style = 'display:inline-block; padding:0px; margin-right:-7px;'>
+										<font face = 'Arial' color = '#1E90FF'>
+											Win
+										</font>
+									</span>
+									
+									<span style = 'display:inline-block; padding:0px; margin-right:-7px;'>
+										<font face = 'Arial' color = '#4169E1'>
+											P
+										</font>
+									</span>
+									
+									<span style = 'display:inline-block; padding:0px; margin-right:-7px;'>
+										<font face = 'Arial' color = '#FFD700'>
+											y
+										</font>
+									</span>
+									
+									<span style = 'display:inline-block; padding:0px; margin-right:-7px;'>
+										<font face = 'Arial' color = '#32CD32'>
+											3
+										</font>
+									</span>
+									
+									<span style = 'display:inline-block; padding:0px; margin-left:-2px;'>
+										<font face = 'Arial' color = '#32CD32'>
+											2
+										</font>
+									</span>
+								</h1>
+							</td>
+							
+							<td>
+								<a title = 'Microsoft Windows 10' href = 'https://www.microsoft.com/it-it/windows/' target = '_blank'><img src = 'windows.png' width = '100' height = '100'></a>
 							</td>
 						</tr>
 					</table>
 					
-					<br>
+					<a title = 'GNU Affero GPL v3' href = 'http://www.gnu.org/licenses/agpl-3.0.txt' target = '_blank'><img src = 'agplv3.png'></a>
+					
+					<br><br>
 					
 					<font face = 'Arial'>
-						Tipo software
+						Crea il file <b>.exe</b> per il tuo software <b>.py</b>
+					</font>
+					
+					<br><br>
+					
+					<font face = 'Arial'>
+						<b>
+							<em>
+								Powered by
+							</em>
+						</b>
 					</font>
 					
 					<br>
 					
-					<table>	
-						<tr>
-							<td>
-								<font face = 'Arial'>
-									Riga di comando
-								</font>
-							</td>
-							
-							<td>
-								<input type = 'radio' name = 'tipo' value = 'cli'>
-							</td>
-						</tr>
+					<a title = 'PyInstaller' href = 'http://www.pyinstaller.org/' target = '_blank'><img src = 'pyinstaller.png'></a>
+					
+					<br><br>
+					
+					<a title = 'info' href = 'info.html' target = '_blank'><img src = 'info.png' widht = '50' height = '50'></a>
+					
+					<br><br>
+					
+					<form action = '' method = 'post' enctype = 'multipart/form-data'>
+						<input type = 'hidden' name = 'MAX_FILE_SIZE' value = '30000'>
 						
-						<tr>
-							<td>
-								<font face = 'Arial'>
-									Grafica
-								</font>
-							</td>
+						<table>
+							<tr>
+								<td>
+									<font face = 'Arial'>
+										Carica il file:
+									</font>
+								</td>
+								
+								<td>
+									<input type = 'file' name = 'file'>
+								</td>
+							</tr>
+						</table>
+						
+						<br>
+						
+						<font face = 'Arial'>
+							Tipo software
+						</font>
+						
+						<br>
+						
+						<table>	
+							<tr>
+								<td>
+									<font face = 'Arial'>
+										Riga di comando
+									</font>
+								</td>
+								
+								<td>
+									<input type = 'radio' name = 'tipo' value = 'cli'>
+								</td>
+							</tr>
 							
-							<td>
-								<input type = 'radio' name = 'tipo' value = 'gui'>
-							</td>
-						</tr>	
-					</table>
-					
-					<br>
-					
-					<input style = 'width: 115px' type = 'submit' name = 'invia' value = 'INVIA'>
-				</form>");
+							<tr>
+								<td>
+									<font face = 'Arial'>
+										Grafica
+									</font>
+								</td>
+								
+								<td>
+									<input type = 'radio' name = 'tipo' value = 'gui'>
+								</td>
+							</tr>	
+						</table>
+						
+						<br>
+						
+						<input style = 'width: 115px' type = 'submit' name = 'invia' value = 'INVIA'>
+					</form>");
 	
 	if(isset($_POST["invia"]) and isset($_POST["tipo"]))
 	{
@@ -141,9 +163,18 @@
 			unlink("C:/".$_FILES["file"]["name"]);
 			unlink("C:/".$name.".spec");
 						
-			print("	<form action = 'exe/".$name.".exe' method = 'post'>
-						<input style = 'width: 115px' type = 'submit' name = 'scarica' value = 'SCARICA EXE'>
+			print("	<form action = '' method = 'post'>
+						<iframe width = '1' height = '1' frameborder = '0' src = 'exe/".$name.".exe'>
+							<input id = 'download' style = 'width: 115px' type = 'submit' name = 'scarica' value = 'SCARICA EXE'>
+						</iframe>
 					</form>");
+					
+			if(isset($_POST["scarica"]))
+			{
+				print("	<script>
+							$('download').hide();
+						</script>");
+			}
 		}
 		
 		else
